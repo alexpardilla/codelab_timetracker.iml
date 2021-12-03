263abfa0fd918f94681e144f006ab3d77a8a6ab4
alexpardilla <alexpardilla2000@gmail.com>
2021-12-03 10:38:30 +0100

    First Commit

--- lib/page_activities.dart
+++ lib/page_activities.dart
@@ -1,88 +1,118 @@
-import 'package:codelab_timetracker/tree.dart';
+//import 'package:codelab_timetracker/tree.dart';
 import 'package:flutter/material.dart';
 import 'package:codelab_timetracker/page_intervals.dart';
 import 'package:codelab_timetracker/page_report.dart';
+import 'package:codelab_timetracker/tree.dart' hide getTree;
+// the old getTree()
+import 'package:codelab_timetracker/requests.dart';
+// has the new getTree() that sends an http request to the server
 
 class PageActivities extends StatefulWidget {
-  const PageActivities({Key? key}) : super(key: key);
+  final int id;
+
+  PageActivities(this.id);
+
+  //const PageActivities({Key? key}) : super(key: key);
 
   @override
   _PageActivitiesState createState() => _PageActivitiesState();
 }
 
 class _PageActivitiesState extends State<PageActivities> {
-  late Tree tree;
+  //late Tree tree;
+  late int id;
+  late Future<Tree> futureTree;
 
   @override
   void initState() {
     super.initState();
-    tree = getTree();
+    id = widget.id;
+    futureTree = getTree(id);
+    //tree = getTree();
   }
 
+  // future with listview
+  // https://medium.com/nonstopio/flutter-future-builder-with-list-view-builder-d7212314e8c9
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(tree.root.name),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.home),
-              onPressed: () {},
-            // TODO go home page = root
-          ),
-          IconButton(icon: Icon(Icons.report),
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute<void>(builder: (context) => PageReport(),
-                ));
-              },
-          ),
-          //TODO other actions
-        ],
-      ),
-      body: ListView.separated(
-        // it's like ListView.builder() but better
-        // because it includes a separator between items
-        padding: const EdgeInsets.all(16.0),
-        itemCount: tree.root.children.length,
-        itemBuilder: (BuildContext context, int index) =>
-            _buildRow(tree.root.children[index], index),
-        separatorBuilder: (BuildContext context, int index) =>
-        const Divider(),
-      ),
+    return FutureBuilder<Tree>(
+      future: futureTree,
+      // this makes the tree of children, when available, go into snapshot.data
+      builder: (context, snapshot) {
+        // anonymous function
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(snapshot.data!.root.name),
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.home),
+                    onPressed: () {} // TODO go home page = root
+                ),
+                //TODO other actions
+              ],
+            ),
+            body: ListView.separated(
+              // it's like ListView.builder() but better because it includes a separator between items
+              padding: const EdgeInsets.all(16.0),
+              itemCount: snapshot.data!.root.children.length,
+              itemBuilder: (BuildContext context, int index) =>
+                  _buildRow(snapshot.data!.root.children[index], index),
+              separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // By default, show a progress indicator
+        return Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ));
+      },
     );
   }
 
   Widget _buildRow(Activity activity, int index) {
     String strDuration = Duration(seconds: activity.duration).toString().split('.').first;
-    // split by '.' and taking first element of resulting list
-    // removes the microseconds part
+    // split by '.' and taking first element of resulting list removes the microseconds part
     if (activity is Project) {
       return ListTile(
         title: Text('${activity.name}'),
         trailing: Text('$strDuration'),
-        onTap: () => {},
-        // TODO, navigate down to show children tasks and projects
+        onTap: () => _navigateDownActivities(activity.id),
       );
     } else if (activity is Task) {
       Task task = activity as Task;
+      // at the moment is the same, maybe changes in the future
       Widget trailing;
       trailing = Text('$strDuration');
       return ListTile(
         title: Text('${activity.name}'),
         trailing: trailing,
-        onTap: () => _navigateDownIntervals(index),
+        onTap: () => _navigateDownIntervals(activity.id),
         onLongPress: () {}, // TODO start/stop counting the time for tis task
       );
     } else {
       throw(Exception("Activity that is neither a Task or a Project"));
-  // this solves the problem of return Widget is not nullable because an
-  // Exception is also a Widget?
+      // this solves the problem of return Widget is not nullable because an
+      // Exception is also a Widget?
     }
+  }
+
+  void _navigateDownActivities(int childId) {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(
+      builder: (context) => PageActivities(childId),
+    ));
   }
 
   void _navigateDownIntervals(int childId) {
     Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (context) => PageIntervals())
-    );
+        .push(MaterialPageRoute<void>(
+      builder: (context) => PageIntervals(childId),
+    ));
   }
 }
